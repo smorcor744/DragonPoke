1e178e4f843a4933bf2aa67f5654ba15f6046d5b
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2024-03-15 16:16:08 +0100

    consola generica

--- src/main/kotlin/Consola.kt
+++ src/main/kotlin/Consola.kt
@@ -1,10 +1,9 @@
 package org.example
 
-class Consola {
-    fun printer(mensaje: String){
+// Declaramos la clase Consola como genérica, con un parámetro de tipo T
+class Consola<T> {
+    // Método para imprimir un mensaje de tipo T
+    fun printer(mensaje: T) {
         println(mensaje)
-    }
-    fun leer(){
-        readln()
     }
 }
